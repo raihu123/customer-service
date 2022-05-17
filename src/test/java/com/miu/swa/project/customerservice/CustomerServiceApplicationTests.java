96cbeaae218ed3a5e77d8eabdad6769716c44da3
rayhanrashid <12345678asdf>
2022-05-17 04:04:57 -0500

    Initial commit

---
+++ src/test/java/com/miu/swa/project/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.swa.project.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
